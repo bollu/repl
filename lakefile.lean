56983a07c27131b72ef4bfcffa04924bd932cea8
Siddharth Bhat <siddu.druid@gmail.com>
2023-06-19 21:18:58 -0700

    add mathlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -9,6 +9,9 @@ lean_lib REPL {
   -- add library configuration options here
 }
 
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "f0dfd500df203fb5a22ed01a4ab0f2dc874e203f"
+
+
 -- Unfortunately the compiled version doesn't work: `unknown package 'Init'`
 @[default_target]
 lean_exe repl where
